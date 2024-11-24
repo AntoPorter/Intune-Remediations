0180f06484bf4645efe7c7092e9a62f9cc5020d1
Anthony Porter <anthonyporter@outlook.com.au>
2024-11-24 11:02:55 +0800

    Update Get-LowDiskSpace

---
+++ ToastNotifications/Get-LowDiskSpace/Remediate_LowDiskSpace.ps1
@@ -0,0 +1,15 @@
+# Define the toast notification content
+$Group = "System Alerts"
+$Title = "Low Disk Space"
+$Message = "Your C: drive is running low on space. Please free up some space to avoid system issues."
+
+# Create the toast notification
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+$template = [Windows.UI.Notifications.ToastTemplateType]::ToastText02
+$toastXml = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent($template)
+$toastTextElements = $toastXml.GetElementsByTagName("text")
+$toastTextElements.Item(0).AppendChild($toastXml.CreateTextNode($Title)) | Out-Null
+$toastTextElements.Item(1).AppendChild($toastXml.CreateTextNode($Message)) | Out-Null
+$toast = [Windows.UI.Notifications.ToastNotification]::new($toastXml)
+$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($Group)
+$notifier.Show($toast)
