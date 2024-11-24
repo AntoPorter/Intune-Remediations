53b168747d40c461f7daa39e0bed43380c5d5ceb
Anthony Porter <anthonyporter@outlook.com.au>
2024-11-24 11:15:28 +0800

    Update Get-PrinterIssues

---
+++ ToastNotifications/Get-PrinterIssues/Remediate_PrinterIssues.ps1
@@ -0,0 +1,15 @@
+# Define the toast notification content
+$Group = "Printer Alerts"
+$Title = "Printer Issues Detected"
+$Message = "There are issues with your printer. Please check the printer status and resolve any errors."
+
+# Create the toast notification
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+$template = [Windows.UI.Notifications.ToastTemplateType]::ToastText02
+$toastXml = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent($template)
+$toastTextElements = $toastXml.GetElementsByTagName("text")
+$toastTextElements.Item(0).AppendChild($toastXml.CreateTextNode($Title)) | Out-Null
+$toastTextElements.Item(1).AppendChild($toastXml.CreateTextNode($Message)) | Out-Null
+$toast = [Windows.UI.Notifications.ToastNotification]::new($toastXml)
+$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($Group)
+$notifier.Show($toast)
